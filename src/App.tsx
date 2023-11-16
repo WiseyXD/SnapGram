f6b8a5f4fba5f4848cf761bf17cbcb31ce6ab5b5
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-17 01:32:19 +0530

    Done with Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import { Home } from "./_root/Pages";
+import RootLayout from "./_root/RootLayout";
 function App() {
 	return (
-		<>
-			<p className="underline">Hello Aryan</p>
-			<div>Aryan</div>
-		</>
+		<main>
+			<Routes>
+				{/* Public Routes */}
+				<Route element={<AuthLayout />}>
+					<Route path="/sign-in" element={<SigninForm />} />
+					<Route path="/sign-up" element={<SignupForm />} />
+				</Route>
+
+				{/* Private Routes */}
+				<Route element={<RootLayout />}>
+					<Route index path="/" element={<Home />} />
+				</Route>
+			</Routes>
+		</main>
 	);
 }
 
