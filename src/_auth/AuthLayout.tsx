559f45c416f9e661ebe3e2f3047b866e5737cf9a
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-19 22:55:03 +0530

    Design of Signup page donw

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
+
 export default function AuthLayout() {
 	const isAuthenticated: boolean = false;
 
@@ -7,12 +8,16 @@ export default function AuthLayout() {
 			{isAuthenticated ? (
 				<Navigate to="/" />
 			) : (
-				<>
+				<div className="flex">
 					<section className="flex flex-1 justify-center items-center flex-col py-10">
 						<Outlet />
-						<img src="" alt="" />
 					</section>
-				</>
+					<img
+						src="/assets/images/side-img.svg"
+						alt="Logo"
+						className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+					/>
+				</div>
 			)}
 		</>
 	);
