53c06f93ec4e93ee0952457079a35cebde1d0900
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-11-19 20:05:46 +0530

    Dinner Break

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,3 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
 export default function AuthLayout() {
-	return <div>Aryan</div>;
+	const isAuthenticated: boolean = false;
+
+	return (
+		<>
+			{isAuthenticated ? (
+				<Navigate to="/" />
+			) : (
+				<>
+					<section className="flex flex-1 justify-center items-center flex-col py-10">
+						<Outlet />
+						<img src="" alt="" />
+					</section>
+				</>
+			)}
+		</>
+	);
 }
